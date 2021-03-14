5580ba05c787d61fd6ea935afaf0f41f6eb2cda6
haiposfja <79170447+haiposfja@users.noreply.github.com>
2021-03-14 19:14:45 -0400

    Delete devilDealMain.java

--- devilDealMain.java
+++
@@ -1,8 +0,0 @@
-/* George Chen and Ryken Santillan
- * Ms. Krasteva
- * 2/17/2021 
- * This program is a subclass of the Rover Class: it adds the attribute of depthLevel and an electricity conductor for measuring salinity. The aquatic rover is a subclass for aquatic celestial bodies.
- */
-public class devilDealMain {
-
-}
